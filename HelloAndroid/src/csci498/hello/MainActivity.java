732a2819aad5b56d2627c6c3809464893e310c91
Vicente Gonzalez <vigonzal@mymail.mines.edu>
2012-08-26 21:41:57 -0600

    Completed the Chapter 7 part of Homework #1

--- HelloAndroid/src/csci498/hello/MainActivity.java
+++ HelloAndroid/src/csci498/hello/MainActivity.java
@@ -2,22 +2,40 @@ package csci498.hello;
 
 import android.os.Bundle;
 import android.app.Activity;
+import android.support.v4.widget.SimpleCursorAdapter.ViewBinder;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+import java.util.Date;
 
-public class MainActivity extends Activity {
-
+public class MainActivity extends Activity implements View.OnClickListener{
+	Button btn;
     @Override
-    public void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+    public void onCreate(Bundle icicle) {
+        super.onCreate(icicle);
+        btn= new Button(this);
+        btn.setOnClickListener(this);
+        updateTime();
+        setContentView(btn);
     }
 
-    @Override
+    private void updateTime() {
+		// TODO Auto-generated method stub
+    	btn.setText(new Date().toString());
+	}
+
+	@Override
     public boolean onCreateOptionsMenu(Menu menu) {
         getMenuInflater().inflate(R.menu.activity_main, menu);
         return true;
     }
+
+	@Override
+	public void onClick(View v) {
+		// TODO Auto-generated method stub
+		updateTime();
+	}
 
     
 }
