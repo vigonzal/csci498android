3b0e56fce7eb9a4400b7dcdabca835142d1f09b4
Vicente Gonzalez <vigonzal@mymail.mines.edu>
2012-08-31 12:10:14 -0600

    should have commited this sooner added the text box resolution/weight.

--- HelloAndroid/src/csci498/hello/MainActivity.java
+++ HelloAndroid/src/csci498/hello/MainActivity.java
@@ -10,30 +10,28 @@ import android.widget.Button;
 import java.util.Date;
 
 public class MainActivity extends Activity implements View.OnClickListener{
+	
 	Button btn;
-    @Override
+    
+	@Override
     public void onCreate(Bundle icicle) {
         super.onCreate(icicle);
-        btn= new Button(this);
+        btn = new Button(this);
         btn.setOnClickListener(this);
         updateTime();
         setContentView(btn);
     }
 
     private void updateTime() {
-		// TODO Auto-generated method stub
     	btn.setText(new Date().toString());
 	}
 
-	@Override
     public boolean onCreateOptionsMenu(Menu menu) {
         getMenuInflater().inflate(R.menu.activity_main, menu);
         return true;
     }
 
-	@Override
 	public void onClick(View v) {
-		// TODO Auto-generated method stub
 		updateTime();
 	}
 
