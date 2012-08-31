0116060df1694fbcfafc0a4c94af628def25b869
vigonzal <vigonzal@mymail.mines.edu>
2012-08-31 14:36:19 -0600

    Added Restaurant.java

---
+++ LunchList/src/csci498/jsmith/lunchlist/Restaurant.java
@@ -0,0 +1,24 @@
+package csci498.jsmith.lunchlist;
+
+public class Restaurant {
+	
+	private String name = " ";
+	private String address = " ";
+	
+	public String getName() {
+		return name;
+	}
+	
+	public void setName(String name) {
+		this.name = name;
+	}
+	
+	public String getAddress() {
+		return address;
+	}
+	
+	public void setAddress(String address) {
+		this.address = address;
+	}
+	
+}
