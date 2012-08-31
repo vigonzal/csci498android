d48c91b4f00c3f234440354c013b06a681ecf512
vigonzal <vigonzal@mymail.mines.edu>
2012-08-31 14:41:48 -0600

    Added the fields for address, name and Button to the Lunchlist.java

--- LunchList/src/csci498/jsmith/lunchlist/LunchList.java
+++ LunchList/src/csci498/jsmith/lunchlist/LunchList.java
@@ -4,15 +4,38 @@ import android.os.Bundle;
 import android.app.Activity;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 import android.support.v4.app.NavUtils;
 
 public class LunchList extends Activity {
+	
+	Restaurant r = new Restaurant();
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_lunch_list);
+        
+        Button save = (Button)findViewById(R.id.save);
+        
+        save.setOnClickListener(onSave);
+        
     }
+    
+    private View.OnClickListener onSave = new View.OnClickListener() {
+		
+		@Override
+		public void onClick(View v) {
+			EditText name = (EditText)findViewById(R.id.name);
+			EditText address = (EditText)findViewById(R.id.adr);
+			
+			r.setName(name.getText().toString());
+			r.setAddress(address.getText().toString());
+			
+		}
+	};
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
