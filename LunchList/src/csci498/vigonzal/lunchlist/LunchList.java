22798f563f44aaf3808e9e612237329837e39925
vigonzal <vigonzal@mymail.mines.edu>
2012-09-10 12:37:57 -0600

    Added show notes as a toast part

--- LunchList/src/csci498/vigonzal/lunchlist/LunchList.java
+++ LunchList/src/csci498/vigonzal/lunchlist/LunchList.java
@@ -4,11 +4,13 @@ import android.app.TabActivity;
 import android.graphics.Color;
 import android.os.Bundle;
 import android.view.Menu;
+import android.view.MenuItem;
 import android.view.MenuInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.view.LayoutInflater;
 import android.widget.AdapterView;
+import android.widget.Toast;
 import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.EditText;
@@ -32,6 +34,7 @@ public class LunchList extends TabActivity {
 	EditText address = null;
 	EditText notes = null;
 	RadioGroup types = null;
+	Restaurant current = null;
 	
 	@Override
 	public void onCreate(Bundle savedInstanceState) {
@@ -77,26 +80,26 @@ public class LunchList extends TabActivity {
 
 		public void onClick(View v) {
 
-			Restaurant r = new Restaurant();
+			current = new Restaurant();
 
-			r.setName(name.getText().toString());
-			r.setAddress(address.getText().toString());
-			r.setNotes(notes.getText().toString());
+			current.setName(name.getText().toString());
+			current.setAddress(address.getText().toString());
+			current.setNotes(notes.getText().toString());
 			
 			switch (types.getCheckedRadioButtonId()) {
 			case R.id.sit_down:
-				r.setType("sit_down");
+				current.setType("sit_down");
 				break;
 			case R.id.take_out:
-				r.setType("take_out");
+				current.setType("take_out");
 				break;
 			case R.id.delivery:
-				r.setType("delivery");
+				current.setType("delivery");
 				break;
 
 			}
 
-			adapter.add(r);
+			adapter.add(current);
 
 		}
 
@@ -169,16 +172,16 @@ public class LunchList extends TabActivity {
 
 		public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
 			
-			Restaurant r=model.get(position);
+			current = model.get(position);
 			
-			name.setText(r.getName());
-			address.setText(r.getAddress());
-			notes.setText(r.getNotes());
+			name.setText(current.getName());
+			address.setText(current.getAddress());
+			notes.setText(current.getNotes());
 			
-			if (r.getType().equals("sit_down")) {
+			if (current.getType().equals("sit_down")) {
 				types.check(R.id.sit_down);
 			}
-			else if (r.getType().equals("take_out")) {
+			else if (current.getType().equals("take_out")) {
 				types.check(R.id.take_out);
 			}
 			else {
@@ -194,6 +197,22 @@ public class LunchList extends TabActivity {
 		new MenuInflater(this).inflate(R.menu.option, menu);
 		return(super.onCreateOptionsMenu(menu));
 		
+	}
+	
+	public boolean onOptionsItemSelected(MenuItem item) {
+		
+		if (item.getItemId() == R.id.toast) {
+			String message = "No restaurant selected";
+			
+			if (current!= null) {
+				message = current.getNotes();
+			}
+			
+			Toast.makeText(this, message, Toast.LENGTH_LONG).show();
+			return(true);
+		}
+		
+		return(super.onOptionsItemSelected(item));
 	}
 
 }
