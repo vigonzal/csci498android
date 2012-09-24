3a08cf1f3f29808410fa9302625c2660ebb5c49d
vigonzal <vigonzal@mymail.mines.edu>
2012-09-24 12:12:50 -0600

    Moved the code from lunchlist to detailForm

--- LunchList/src/csci498/vigonzal/lunchlist/DetailForm.java
+++ LunchList/src/csci498/vigonzal/lunchlist/DetailForm.java
@@ -2,13 +2,51 @@ package csci498.vigonzal.lunchlist;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.app.Activity;
+import android.database.Cursor;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.RadioGroup;
+import android.widget.TextView;
 
 public class DetailForm extends Activity{
+	
+	EditText name=null;
+	EditText address=null;
+	EditText notes=null;
+	RadioGroup types=null;
+	RestaurantHelper helper=null;
 	
 	@Override
 	public void onCreate(Bundle savedInstanceState){
 		super.onCreate(savedInstanceState);
 		
+		setContentView(R.layout.detail_form);
+		
+		helper=new RestaurantHelper(this);
+		name=(EditText)findViewById(R.id.name);
+		address=(EditText)findViewById(R.id.addr);
+		notes=(EditText)findViewById(R.id.notes);
+		types=(RadioGroup)findViewById(R.id.types);
+		Button save=(Button)findViewById(R.id.save);
+		save.setOnClickListener(onSave);
 	}
-
+	private View.OnClickListener onSave=new View.OnClickListener() {
+		public void onClick(View v) {
+			String type=null;
+			switch (types.getCheckedRadioButtonId()) {
+			case R.id.sit_down:
+				type="sit_down";
+				break;
+			case R.id.take_out:
+				type="take_out";
+				break;
+			case R.id.delivery:
+				type="delivery";
+				break;
+			}
+		}
+	};
 }
