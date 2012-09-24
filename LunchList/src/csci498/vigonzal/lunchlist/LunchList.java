3cc211313622479a96e0970133d10329e2a77eed
vigonzal <vigonzal@mymail.mines.edu>
2012-09-24 12:36:25 -0600

    Updated teh Lunchlist OnCreate method

--- LunchList/src/csci498/vigonzal/lunchlist/LunchList.java
+++ LunchList/src/csci498/vigonzal/lunchlist/LunchList.java
@@ -1,5 +1,6 @@
 package csci498.vigonzal.lunchlist;
 
+import android.app.ListActivity;
 import android.app.TabActivity;
 import android.content.Context;
 import android.content.Intent;
@@ -33,7 +34,7 @@ import org.apache.http.protocol.RequestContent;
 
 
 @SuppressWarnings("deprecation")
-public class LunchList extends TabActivity {
+public class LunchList extends ListActivity {
 
 	Cursor model;
 	
@@ -49,41 +50,12 @@ public class LunchList extends TabActivity {
 	public void onCreate(Bundle savedInstanceState) {
 
 		super.onCreate(savedInstanceState);
-		setContentView(R.layout.activity_lunch_list);
-		
-		Button save = (Button)findViewById(R.id.save);
+		setContentView(R.layout.main);
 		helper = new RestaurantHelper(this);
-		
-		name = (EditText)findViewById(R.id.name);
-		address = (EditText)findViewById(R.id.addr);
-		notes = (EditText)findViewById(R.id.notes);
-		types = (RadioGroup)findViewById(R.id.types);
-		
-		
-		save.setOnClickListener(onSave);
-		ListView list = (ListView)findViewById(R.id.restaurants);
-		
 		model = helper.getAll();
 		startManagingCursor(model);
 		adapter = new RestaurantAdapter(model);
-		list.setAdapter(adapter);
-
-		TabHost.TabSpec spec = getTabHost().newTabSpec("tag1");
-		
-		spec.setContent(R.id.restaurants);
-		spec.setIndicator("List", getResources().getDrawable(R.drawable.list));
-		
-		getTabHost().addTab(spec);
-		
-		spec = getTabHost().newTabSpec("tag2");
-		spec.setContent(R.id.details);
-		spec.setIndicator("Details", getResources().getDrawable(R.drawable.restaurant));
-		
-		getTabHost().addTab(spec);
-		
-		getTabHost().setCurrentTab(0);
-		
-		list.setOnItemClickListener(onListClick);
+		setListAdapter(adapter);
 
 	}
 	
@@ -190,23 +162,6 @@ public class LunchList extends TabActivity {
 			
 			startActivity(i);
 			
-//			model.moveToPosition(position);
-//			
-//			name.setText(helper.getName(model));
-//			address.setText(helper.getAddress(model));
-//			notes.setText(helper.getNotes(model));
-//			
-//			if (helper.getType(model).equals("sit_down")) {
-//				types.check(R.id.sit_down);
-//			}
-//			else if (helper.getType(model).equals("take_out")) {
-//				types.check(R.id.take_out);
-//			}
-//			else {
-//				types.check(R.id.delivery);
-//			}
-//			
-//			getTabHost().setCurrentTab(1);
 		}
 	};
 	
