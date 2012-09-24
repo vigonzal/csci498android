1ed6a819ff353222837b85db96f5381270af911b
vigonzal <vigonzal@mymail.mines.edu>
2012-09-24 12:05:34 -0600

    Added the launch stub activity on listClick

--- LunchList/src/csci498/vigonzal/lunchlist/LunchList.java
+++ LunchList/src/csci498/vigonzal/lunchlist/LunchList.java
@@ -2,6 +2,7 @@ package csci498.vigonzal.lunchlist;
 
 import android.app.TabActivity;
 import android.content.Context;
+import android.content.Intent;
 import android.database.Cursor;
 import android.graphics.Color;
 import android.os.Bundle;
@@ -185,23 +186,27 @@ public class LunchList extends TabActivity {
 
 		public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
 			
-			model.moveToPosition(position);
+			Intent i =  new Intent(LunchList.this, DetailForm.class);
 			
-			name.setText(helper.getName(model));
-			address.setText(helper.getAddress(model));
-			notes.setText(helper.getNotes(model));
+			startActivity(i);
 			
-			if (helper.getType(model).equals("sit_down")) {
-				types.check(R.id.sit_down);
-			}
-			else if (helper.getType(model).equals("take_out")) {
-				types.check(R.id.take_out);
-			}
-			else {
-				types.check(R.id.delivery);
-			}
-			
-			getTabHost().setCurrentTab(1);
+//			model.moveToPosition(position);
+//			
+//			name.setText(helper.getName(model));
+//			address.setText(helper.getAddress(model));
+//			notes.setText(helper.getNotes(model));
+//			
+//			if (helper.getType(model).equals("sit_down")) {
+//				types.check(R.id.sit_down);
+//			}
+//			else if (helper.getType(model).equals("take_out")) {
+//				types.check(R.id.take_out);
+//			}
+//			else {
+//				types.check(R.id.delivery);
+//			}
+//			
+//			getTabHost().setCurrentTab(1);
 		}
 	};
 	
