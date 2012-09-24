06d2610d11707cadaf3adb046f4fc1c02463749a
vigonzal <vigonzal@mymail.mines.edu>
2012-09-24 11:58:18 -0600

    Added DetailForm.java

---
+++ LunchList/src/csci498/vigonzal/lunchlist/DetailForm.java
@@ -0,0 +1,14 @@
+package csci498.vigonzal.lunchlist;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class DetailForm extends Activity{
+	
+	@Override
+	public void onCreate(Bundle savedInstanceState){
+		super.onCreate(savedInstanceState);
+		
+	}
+
+}
