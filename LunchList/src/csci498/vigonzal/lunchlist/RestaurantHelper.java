dd14bcd1075edc7f8adc6335f8fad42df1c574d9
vigonzal <vigonzal@mymail.mines.edu>
2012-10-21 16:52:31 -0600

    Added the get mothods in Restaurant Helper so that we could get all the data from the DB or parts of the restaurant data.

--- LunchList/src/csci498/vigonzal/lunchlist/RestaurantHelper.java
+++ LunchList/src/csci498/vigonzal/lunchlist/RestaurantHelper.java
@@ -38,4 +38,26 @@ class RestaurantHelper extends SQLiteOpenHelper {
 			cv.put("notes", notes);
 			getWritableDatabase().insert("restaurants", "name", cv);
 	}
+
+	public Cursor getAll(){
+		return getReadableDatabase().rawQuery("SELECT _id, name, address, type," +
+				" notes FROM restaurants ORDER BY name", null);
+	}
+	
+	public String getName(Cursor c) {
+		return(c.getString(1));
+	}
+	
+	public String getAddress(Cursor c) {
+		return(c.getString(2));
+	}
+	
+	public String getType(Cursor c) {
+		return(c.getString(3));
+	}
+	
+	public String getNotes(Cursor c) {
+		return(c.getString(4));
+	}
+	
 }
