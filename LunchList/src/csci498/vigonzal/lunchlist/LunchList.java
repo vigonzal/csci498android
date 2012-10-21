dab2facb6311a7a069d6c37a7f4f197989eac698
vigonzal <vigonzal@mymail.mines.edu>
2012-10-21 16:09:15 -0600

    changed and moved things around for consistency, also: Added the atomicboolean isActive, changed the loop in longTask to check the boolean state

--- LunchList/src/csci498/vigonzal/lunchlist/LunchList.java
+++ LunchList/src/csci498/vigonzal/lunchlist/LunchList.java
@@ -23,6 +23,7 @@ import android.widget.TabHost;
 import android.widget.TextView;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.concurrent.atomic.AtomicBoolean;
 
 
 @SuppressWarnings("deprecation")
@@ -30,7 +31,7 @@ public class LunchList extends TabActivity {
 
 	List<Restaurant> model = new ArrayList<Restaurant>();
 	ArrayList<Restaurant> restaurants = new ArrayList<Restaurant>();
-	
+	AtomicBoolean isActive = new AtomicBoolean(true);
 	RestaurantAdapter adapter;
 	Restaurant current;
 	RadioGroup types;
@@ -38,6 +39,63 @@ public class LunchList extends TabActivity {
 	EditText notes;
 	EditText name;
 	int progress;
+	
+	private View.OnClickListener onSave = new View.OnClickListener() {
+		public void onClick(View v) {
+			current = new Restaurant();
+			current.setName(name.getText().toString());
+			current.setAddress(address.getText().toString());
+			current.setNotes(notes.getText().toString());
+			
+			switch (types.getCheckedRadioButtonId()) {
+			case R.id.sit_down:
+				current.setType("sit_down");
+				break;
+			case R.id.take_out:
+				current.setType("take_out");
+				break;
+			case R.id.delivery:
+				current.setType("delivery");
+				break;
+			}
+			adapter.add(current);
+		}
+	};
+	
+	private AdapterView.OnItemClickListener onListClick = new AdapterView.OnItemClickListener() {
+		public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+			current = model.get(position);
+			name.setText(current.getName());
+			address.setText(current.getAddress());
+			notes.setText(current.getNotes());
+			
+			if (current.getType().equals("sit_down")) {
+				types.check(R.id.sit_down);
+			}
+			else if (current.getType().equals("take_out")) {
+				types.check(R.id.take_out);
+			}
+			else {
+				types.check(R.id.delivery);
+			}
+			
+			getTabHost().setCurrentTab(1);
+		}
+	};
+	
+	private Runnable longTask = new Runnable(){
+		public void run(){
+			for (int i = progress; i < 10000 && isActive.get(); i +=200) {
+				doSomeLongWork(200);
+			}
+			
+			runOnUiThread(new Runnable(){
+				public void run(){
+					setProgressBarVisibility(false);
+				}
+			});
+		}
+	};
 	
 	@Override
 	public void onCreate(Bundle savedInstanceState) {
@@ -67,29 +125,45 @@ public class LunchList extends TabActivity {
 		getTabHost().setCurrentTab(0);
 		list.setOnItemClickListener(onListClick);
 	}
-	
-	private View.OnClickListener onSave = new View.OnClickListener() {
-		public void onClick(View v) {
-			current = new Restaurant();
-			current.setName(name.getText().toString());
-			current.setAddress(address.getText().toString());
-			current.setNotes(notes.getText().toString());
-			
-			switch (types.getCheckedRadioButtonId()) {
-			case R.id.sit_down:
-				current.setType("sit_down");
-				break;
-			case R.id.take_out:
-				current.setType("take_out");
-				break;
-			case R.id.delivery:
-				current.setType("delivery");
-				break;
-			}
-			adapter.add(current);
-		}
-	};
 
+	@Override
+	public boolean onCreateOptionsMenu(Menu menu) {
+		new MenuInflater(this).inflate(R.menu.option, menu);
+		return(super.onCreateOptionsMenu(menu));
+	}
+	
+	public boolean onOptionsItemSelected(MenuItem item) {
+		
+		if (item.getItemId() == R.id.toast) {
+			String message = "No restaurant selected";
+			
+			if (current!= null) {
+				message = current.getNotes();
+			}
+			
+			Toast.makeText(this, message, Toast.LENGTH_LONG).show();
+			return(true);
+		}
+		else if (item.getItemId() == R.id.run){
+			setProgressBarVisibility(true);
+			progress = 0;
+			new Thread(longTask).start();
+			return true;
+		}
+		return(super.onOptionsItemSelected(item));
+	}
+	
+	private void doSomeLongWork(final int incr){
+		runOnUiThread(new Runnable() {
+			public void run() {
+				progress += incr;
+				setProgress(progress);
+			}
+		});
+		//note that this should be something more useful in the future.
+		SystemClock.sleep(250);
+	}
+	
 	class RestaurantAdapter extends ArrayAdapter<Restaurant> {
 		public RestaurantAdapter() {
 			super(LunchList.this, android.R.layout.simple_list_item_1, model);
@@ -145,81 +219,4 @@ public class LunchList extends TabActivity {
 			}
 		}
 	}
-
-
-	private AdapterView.OnItemClickListener onListClick = new AdapterView.OnItemClickListener() {
-		public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
-			current = model.get(position);
-			name.setText(current.getName());
-			address.setText(current.getAddress());
-			notes.setText(current.getNotes());
-			
-			if (current.getType().equals("sit_down")) {
-				types.check(R.id.sit_down);
-			}
-			else if (current.getType().equals("take_out")) {
-				types.check(R.id.take_out);
-			}
-			else {
-				types.check(R.id.delivery);
-			}
-			
-			getTabHost().setCurrentTab(1);
-		}
-	};
-	
-	@Override
-	public boolean onCreateOptionsMenu(Menu menu) {
-		new MenuInflater(this).inflate(R.menu.option, menu);
-		return(super.onCreateOptionsMenu(menu));
-	}
-	
-	public boolean onOptionsItemSelected(MenuItem item) {
-		
-		if (item.getItemId() == R.id.toast) {
-			String message = "No restaurant selected";
-			
-			if (current!= null) {
-				message = current.getNotes();
-			}
-			
-			Toast.makeText(this, message, Toast.LENGTH_LONG).show();
-			return(true);
-		}
-		else if (item.getItemId() == R.id.run){
-			setProgressBarVisibility(true);
-			progress = 0;
-			new Thread(longTask).start();
-			return true;
-		}
-		return(super.onOptionsItemSelected(item));
-	}
-	
-	private void doSomeLongWork(final int incr){
-		runOnUiThread(new Runnable() {
-			public void run() {
-				progress += incr;
-				setProgress(progress);
-			}
-		});
-		//note that this should be something more useful in the future.
-		SystemClock.sleep(250);
-	}
-	
-	private Runnable longTask = new Runnable(){
-		public void run(){
-			for (int i=progress;
-					i<10000;
-					i+=200) {
-				doSomeLongWork(200);
-			}
-			
-			runOnUiThread(new Runnable(){
-				public void run(){
-					setProgressBarVisibility(false);
-				}
-			});
-		}
-	};
-
 }
