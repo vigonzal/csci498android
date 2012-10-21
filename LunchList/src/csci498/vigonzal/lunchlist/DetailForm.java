88f0d58b46a2c4e91da23331772bd76d57535b75
vigonzal <vigonzal@mymail.mines.edu>
2012-10-21 17:40:57 -0600

    Added DetailForm activity to the Manifest,Changed OnItemClickListener to make use of this activity.

--- LunchList/src/csci498/vigonzal/lunchlist/DetailForm.java
+++ LunchList/src/csci498/vigonzal/lunchlist/DetailForm.java
@@ -8,7 +8,7 @@ public class DetailForm extends Activity {
 	@Override
 	public void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
-		// setContentView(R.layout.main);
+		// setContentView(R.layout.DetailForm);
 	}
 	
 }
