459f383ec7ab9046cdce34532253614e37b97b43
vigonzal <vigonzal@mymail.mines.edu>
2012-10-21 19:10:00 -0600

    added the image for the menu add option, added to lunchlist the onOptionsItemSelected method with the OnCreateOptionsMenu method as well. Changed the options xml to match the new option and get rid of the old ones.

--- LunchList/src/csci498/vigonzal/lunchlist/LunchList.java
+++ LunchList/src/csci498/vigonzal/lunchlist/LunchList.java
@@ -75,6 +75,21 @@ public class LunchList extends ListActivity {
 		helper.close();
 	}
 	
+	@Override
+	public boolean onCreateOptionsMenu(Menu menu) {
+		new MenuInflater(this).inflate(R.menu.option, menu);
+		return super.onCreateOptionsMenu(menu);
+	}
+	
+	@Override
+	public boolean onOptionsItemSelected(MenuItem item) {
+		if (item.getItemId()==R.id.add) {
+			startActivity(new Intent(LunchList.this, DetailForm.class));
+			return(true);
+		}
+		return(super.onOptionsItemSelected(item));
+	}
+	
 	class RestaurantAdapter extends CursorAdapter {
 		RestaurantAdapter(Cursor c){
 			super(LunchList.this, c);
