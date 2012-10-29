7e4a65111d61a2ba4acce56c539a3b19d8830b88
vigonzal <vigonzal@mymail.mines.edu>
2012-10-28 19:25:05 -0600

    added the prefs item to options.xml added functionality else clause to OnOptionItem Selected so that the menu will start the activity when clicked, Added the image for the preferences.

--- LunchList/src/csci498/vigonzal/lunchlist/LunchList.java
+++ LunchList/src/csci498/vigonzal/lunchlist/LunchList.java
@@ -77,9 +77,12 @@ public class LunchList extends ListActivity {
 	public boolean onOptionsItemSelected(MenuItem item) {
 		if (item.getItemId() == R.id.add) {
 			startActivity(new Intent(LunchList.this, DetailForm.class));
-			return(true);
+			return true;
+		}else if(item.getItemId() == R.id.prefs){
+			startActivity(new Intent(LunchList.this, EditPreferences.class));
+			return true;
 		}
-		return(super.onOptionsItemSelected(item));
+		return super.onOptionsItemSelected(item);
 	}
 	
 	class RestaurantAdapter extends CursorAdapter {
