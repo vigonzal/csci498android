1dc98410d2c8da775752f150bce28780c16c77fa
vigonzal <vigonzal@mymail.mines.edu>
2012-11-01 17:59:16 -0600

    Added the oncreate method to feed activity so now we have an instance created, added two members, state and the FEED_URL. Added onRetainNonConfigurationInstance so that if we rotate we will get back our feed url otherwise if is null we crete the feed url. and added setFeed so that we can now set the feed to whatever we wanted.
    APT 15 Completed

--- LunchList/src/csci498/vigonzal/lunchlist/FeedActivity.java
+++ LunchList/src/csci498/vigonzal/lunchlist/FeedActivity.java
@@ -6,13 +6,102 @@ import org.apache.http.client.methods.HttpGet;
 import org.apache.http.impl.client.BasicResponseHandler;
 import org.apache.http.impl.client.DefaultHttpClient;
 import org.mcsoxford.rss.RSSFeed;
+import org.mcsoxford.rss.RSSItem;
 import org.mcsoxford.rss.RSSReader;
 import android.app.AlertDialog;
 import android.app.ListActivity;
 import android.os.AsyncTask;
+import android.os.Bundle;
 import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.BaseAdapter;
+import android.widget.TextView;
 
 public class FeedActivity extends ListActivity {
+
+	private InstanceState state;
+	public static final String FEED_URL="apt.tutorial.FEED_URL";
+	
+	@SuppressWarnings("deprecation")
+	@Override
+	public void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		state = (InstanceState)getLastNonConfigurationInstance();
+		if (state == null) {
+			state = new InstanceState();
+			state.task = new FeedTask(this);
+			state.task.execute(getIntent().getStringExtra(FEED_URL));
+		}
+		else {
+			if (state.task != null) {
+				state.task.attach(this);
+			}
+			if (state.feed != null) {
+				setFeed(state.feed);
+			}
+		}
+	}
+	
+	@Override
+	public Object onRetainNonConfigurationInstance() {
+		if (state.task != null) {
+			state.task.detach();
+		}
+		return state;
+	}
+	
+	private void setFeed(RSSFeed feed) {
+		state.feed = feed;
+		setListAdapter(new FeedAdapter(feed));
+	}
+
+	private void goBlooey(Throwable t) {
+		AlertDialog.Builder builder = new AlertDialog.Builder(this);
+		builder
+		.setTitle("Exception!")
+		.setMessage(t.toString())
+		.setPositiveButton("OK", null)
+		.show();
+	}
+
+	private class FeedAdapter extends BaseAdapter {
+		RSSFeed feed = null;
+		FeedAdapter(RSSFeed feed) {
+			super();
+			this.feed = feed;
+		}
+		public int getCount() {
+			return feed.getItems().size();
+		}
+
+		public Object getItem(int position) {
+			return feed.getItems().get(position);
+		}
+
+		public long getItemId(int position) {
+			return position;
+		}
+
+		public View getView(int position, View convertView, ViewGroup parent) {
+			View row = convertView;
+			if (row == null) {
+				LayoutInflater inflater = getLayoutInflater();
+				row = inflater.inflate(android.R.layout.simple_list_item_1, parent, false);
+			}
+
+			RSSItem item = (RSSItem)getItem(position);
+			((TextView)row).setText(item.getTitle());
+			return row;
+		}
+	}
+
+	private static class InstanceState {
+		RSSFeed feed = null;
+		FeedTask task = null;
+	}
+
 	private static class FeedTask extends AsyncTask<String, Void, RSSFeed> {
 		private RSSReader reader = new RSSReader();
 		private Exception e = null;
@@ -48,14 +137,5 @@ public class FeedActivity extends ListActivity {
 			}
 		}
 	}
-
-	private void goBlooey(Throwable t) {
-	AlertDialog.Builder builder = new AlertDialog.Builder(this);
-	builder
-	.setTitle("Exception!")
-	.setMessage(t.toString())
-	.setPositiveButton("OK", null)
-	.show();
-}
 
 }
