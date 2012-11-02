e1a83b9313615291808f41c9f4d1493579caad91
vigonzal <vigonzal@mymail.mines.edu>
2012-11-02 12:10:40 -0600

    Updated Restaurant Helper so that it will now be at DB version 3, and have new getters for lattitude and longitude. Also made it so the SCHEMA includes them too in the restaurant Table.

--- LunchList/src/csci498/vigonzal/lunchlist/RestaurantHelper.java
+++ LunchList/src/csci498/vigonzal/lunchlist/RestaurantHelper.java
@@ -9,7 +9,7 @@ import android.database.sqlite.SQLiteOpenHelper;
 class RestaurantHelper extends SQLiteOpenHelper {
 
 	private static final String DATABASE_NAME="lunchlist.db";
-	private static final int SCHEMA_VERSION = 2;
+	private static final int SCHEMA_VERSION = 3;
 
 	public RestaurantHelper(Context context) {
 		super(context, DATABASE_NAME, null, SCHEMA_VERSION);
@@ -18,23 +18,29 @@ class RestaurantHelper extends SQLiteOpenHelper {
 	@Override
 	public void onCreate(SQLiteDatabase db) {
 		db.execSQL("CREATE TABLE restaurants (_id INTEGER PRIMARY KEY AUTOINCREMENT," +
-				"name TEXT, address TEXT, type TEXT, notes TEXT, feed TEXT);");
+				"name TEXT, address TEXT, type TEXT, notes TEXT, feed TEXT, lat REAL, lon REAL);");
 	}
 
 	@Override
 	public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
-		db.execSQL("ALTER TABLE restaurants ADD COLUMN feed TEXT");
+		if (oldVersion < 2) {
+			db.execSQL("ALTER TABLE restaurants ADD COLUMN feed TEXT");
+		}
+		if (oldVersion < 3) {
+			db.execSQL("ALTER TABLE restaurants ADD COLUMN lat REAL");
+			db.execSQL("ALTER TABLE restaurants ADD COLUMN lon REAL");
+		}
 	}
 
 	public void insert(String name, String address,
 			String type, String notes, String feed) {
-			ContentValues cv=new ContentValues();
-			cv.put("name", name);
-			cv.put("address", address);
-			cv.put("type", type);
-			cv.put("notes", notes);
-			cv.put("feed", feed);
-			getWritableDatabase().insert("restaurants", "name", cv);
+		ContentValues cv=new ContentValues();
+		cv.put("name", name);
+		cv.put("address", address);
+		cv.put("type", type);
+		cv.put("notes", notes);
+		cv.put("feed", feed);
+		getWritableDatabase().insert("restaurants", "name", cv);
 	}
 
 	public void update(String id, String name, String address, String type, String notes, String feed) {
@@ -47,37 +53,52 @@ class RestaurantHelper extends SQLiteOpenHelper {
 		cv.put("feed", feed);
 		getWritableDatabase().update("restaurants", cv, "_ID=?", args);
 	}
-	
-	public Cursor getAll(String orderBy){
-		return getReadableDatabase().rawQuery("SELECT _id, name, address, type," +
-				" notes, feed FROM restaurants ORDER BY " + orderBy, null);
+
+	public void updateLocation(String id, double lat, double lon) {
+		ContentValues cv = new ContentValues();
+		String[] args = {id};
+		cv.put("lat", lat);
+		cv.put("lon", lon);
+		getWritableDatabase().update("restaurants", cv, "_ID=?", args);
 	}
-	
-	public String getName(Cursor c) {
-		return c.getString(1);
-	}
-	
-	public String getAddress(Cursor c) {
-		return c.getString(2);
-	}
-	
-	public String getType(Cursor c) { 
-		return c.getString(3);
-	}
-	
-	public String getNotes(Cursor c) {
-		return c.getString(4);
-	}
-	
-	public String getFeed(Cursor c) {
-		return c.getString(5);
-	}
-	
+
 	public Cursor getById(String id) {
 		String[] args={id};
 		return(getReadableDatabase()
-				.rawQuery("SELECT _id, name, address, type, notes, feed FROM " +
+				.rawQuery("SELECT _id, name, address, type, notes, feed, lat, lon FROM " +
 						"restaurants WHERE _ID=?", args));
+	}
+
+	public Cursor getAll(String orderBy){
+		return getReadableDatabase().rawQuery("SELECT _id, name, address, type," +
+				" notes, feed, lat, lon FROM restaurants ORDER BY " + orderBy, null);
+	}
+
+	public String getName(Cursor c) {
+		return c.getString(1);
+	}
+
+	public String getAddress(Cursor c) {
+		return c.getString(2);
+	}
+
+	public String getType(Cursor c) { 
+		return c.getString(3);
+	}
+
+	public String getNotes(Cursor c) {
+		return c.getString(4);
+	}
+
+	public String getFeed(Cursor c) {
+		return c.getString(5);
+	}
+
+	public double getLatitude(Cursor c) {
+		return c.getDouble(6);
+	}
+	public double getLongitude(Cursor c) {
+		return c.getDouble(7);
 	}
 
 }
