3ef0f74ada294883dda3fd13d8654c9799d9ca30
vigonzal <vigonzal@mymail.mines.edu>
2012-11-02 11:17:29 -0600

    Added the member EXTRA_URL, which is the key to our Intent extra and will identify the feed URL. Added the implementation of onHandleIntent so thatit will be called on a background thread.

--- LunchList/src/csci498/vigonzal/lunchlist/FeedService.java
+++ LunchList/src/csci498/vigonzal/lunchlist/FeedService.java
@@ -1,14 +1,28 @@
 package csci498.vigonzal.lunchlist;
 
+import org.mcsoxford.rss.RSSFeed;
+import org.mcsoxford.rss.RSSReader;
+
 import android.app.IntentService;
 import android.content.Intent;
+import android.util.Log;
 
 public class FeedService extends IntentService {
+	
+	public static final String EXTRA_URL="apt.tutorial.EXTRA_URL";
+	
 	public FeedService() {
 		super("FeedService");
 	}
+	
 	@Override
 	public void onHandleIntent(Intent i) {
-		// do something
+		RSSReader reader = new RSSReader();
+		try {
+			RSSFeed result = reader.load(i.getStringExtra(EXTRA_URL));
+		}
+		catch (Exception e) {
+			Log.e("LunchList", "Exception parsing feed", e);
+		}
 	}
 }
