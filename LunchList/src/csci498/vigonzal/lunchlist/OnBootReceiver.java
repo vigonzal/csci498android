47313ef9eecd5b20f9bc068ee5e4f413af50edb3
vigonzal <vigonzal@mymail.mines.edu>
2012-11-03 01:21:26 -0600

    Added the onBootReceiver so that we can not lose our data on boot i think, and then we added the permissions and the intent filter and receiver to the maniofest.

---
+++ LunchList/src/csci498/vigonzal/lunchlist/OnBootReceiver.java
@@ -0,0 +1,14 @@
+package csci498.vigonzal.lunchlist;
+
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+
+public class OnBootReceiver extends BroadcastReceiver{
+
+	@Override
+	public void onReceive(Context ctxt, Intent intent) {
+	// do something
+	}
+	
+}
