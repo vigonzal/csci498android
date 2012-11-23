cd4a358a32b9d5cd3d8fdb01073cbd6491cf5046
vigonzal <vigonzal@mymail.mines.edu>
2012-11-22 19:34:14 -0700

    Added the new Service to the manifest so that we can have a listview widget in higher android versions like 3.0 where it is supported. updated the oncreate in AppWidget to check the .0 of the android software. Added the default row in wdget row xml from the sdk so that the list will have a contrast to the background of the user desktop. Added the widget xml and the widget provider so that they both support the new widget layout for listview in the 3.0 and better OSs Implemented ListViewsFactory and ListWidget Service

---
+++ LunchList/src/csci498/vigonzal/lunchlist/ListWidgetService.java
@@ -0,0 +1,12 @@
+package csci498.vigonzal.lunchlist;
+
+import android.content.Intent;
+import android.widget.RemoteViewsService;
+
+public class ListWidgetService extends RemoteViewsService {
+	@Override
+	public RemoteViewsFactory onGetViewFactory(Intent intent) {
+		return new ListViewsFactory(this.getApplicationContext(), intent);
+	}
+
+}
