8c4684ebfc8981bf49b04dd5bbc530034c24166c
vigonzal <vigonzal@mymail.mines.edu>
2012-11-24 23:53:17 -0700

    Added consistencies after I couldnt find a bug in the code so copy pasted and found difference.

--- LunchList/src/csci498/vigonzal/lunchlist/DetailFragment.java
+++ LunchList/src/csci498/vigonzal/lunchlist/DetailFragment.java
@@ -22,47 +22,52 @@ import android.widget.TextView;
 import android.widget.Toast;
 
 public class DetailFragment extends Fragment {
-	EditText name=null;
-	EditText address=null;
-	EditText notes=null;
-	EditText feed=null;
-	RadioGroup types=null;
-	RestaurantHelper helper=null;
-	String restaurantId=null;
-	TextView location=null;
-	LocationManager locMgr=null;
+	
+	EditText name;
+	EditText address;
+	EditText notes;
+	EditText feed;
+	RadioGroup types;
+	RestaurantHelper helper;
+	String restaurantId;
+	TextView location;
+	LocationManager locMgr;
 	double latitude=0.0d;
 	double longitude=0.0d;
+	
 	@Override
 	public void onCreate(Bundle state) {
 		super.onCreate(state);
 		setHasOptionsMenu(true);
 	}
+	
 	@Override
-	public View onCreateView(LayoutInflater inflater, ViewGroup container,
-			Bundle savedInstanceState) {
-		return(inflater.inflate(R.layout.detail_form, container, false));
+	public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+		return inflater.inflate(R.layout.detail_form, container, false);
 	}
+	
 	@Override
 	public void onActivityCreated(Bundle savedInstanceState) {
 		super.onActivityCreated(savedInstanceState);
-		locMgr=(LocationManager)getActivity().
-				getSystemService(Context.LOCATION_SERVICE);
-		name=(EditText)getView().findViewById(R.id.name);
-		address=(EditText)getView().findViewById(R.id.addr);
-		notes=(EditText)getView().findViewById(R.id.notes);
-		types=(RadioGroup)getView().findViewById(R.id.types);
-		feed=(EditText)getView().findViewById(R.id.feed);
-		location=(TextView)getView().findViewById(R.id.location);
-	}@Override
+		locMgr = (LocationManager)getActivity().getSystemService(Context.LOCATION_SERVICE);
+		name = (EditText)getView().findViewById(R.id.name);
+		address = (EditText)getView().findViewById(R.id.addr);
+		notes = (EditText)getView().findViewById(R.id.notes);
+		types = (RadioGroup)getView().findViewById(R.id.types);
+		feed = (EditText)getView().findViewById(R.id.feed);
+		location = (TextView)getView().findViewById(R.id.location);
+	}
+	
+	@Override
 	public void onResume() {
 		super.onResume();
-		helper=new RestaurantHelper(getActivity());
-		restaurantId=getActivity().getIntent().getStringExtra(LunchList.ID_EXTRA);
-		if (restaurantId!=null) {
+		helper = new RestaurantHelper(getActivity());
+		restaurantId = getActivity().getIntent().getStringExtra(LunchList.ID_EXTRA);
+		if (restaurantId != null) {
 			load();
 		}
 	}
+	
 	@Override
 	public void onPause() {
 		save();
@@ -70,58 +75,63 @@ public class DetailFragment extends Fragment {
 		locMgr.removeUpdates(onLocationChange);
 		super.onPause();
 	}
+	
 	@Override
 	public void onCreateOptionsMenu(Menu menu, MenuInflater inflater) {
 		inflater.inflate(R.menu.details_option, menu);
 	}
+	
 	@Override
 	public void onPrepareOptionsMenu(Menu menu) {
-		if (restaurantId==null) {
+		if (restaurantId == null) {
 			menu.findItem(R.id.location).setEnabled(false);
 			menu.findItem(R.id.map).setEnabled(false);
 		}
 	}
+	
 	@Override
 	public boolean onOptionsItemSelected(MenuItem item) {
-		if (item.getItemId()==R.id.feed) {
+		if (item.getItemId() == R.id.feed) {
 			if (isNetworkAvailable()) {
-				Intent i=new Intent(getActivity(), FeedActivity.class);
+				Intent i = new Intent(getActivity(), FeedActivity.class);
 				i.putExtra(FeedActivity.FEED_URL, feed.getText().toString());
 				startActivity(i);
 			}
 			else {
 				Toast
-				.makeText(getActivity(), "Sorry, the Internet is not available",
-						Toast.LENGTH_LONG)
-						.show();
+					.makeText(getActivity(), "Sorry, the Internet is not available", Toast.LENGTH_LONG)
+					.show();
 			}
-			return(true);
-		}else if (item.getItemId()==R.id.location) {
-			locMgr.requestLocationUpdates(LocationManager.GPS_PROVIDER,0, 0, onLocationChange);return(true);
+			return true;
+		}else if (item.getItemId() == R.id.location) {
+			locMgr.requestLocationUpdates(LocationManager.GPS_PROVIDER,0, 0, onLocationChange);
+			return true;
 		}
-		else if (item.getItemId()==R.id.map) {
-			Intent i=new Intent(getActivity(), RestaurantMap.class);
+		else if (item.getItemId() == R.id.map) {
+			Intent i = new Intent(getActivity(), RestaurantMap.class);
 			i.putExtra(RestaurantMap.EXTRA_LATITUDE, latitude);
 			i.putExtra(RestaurantMap.EXTRA_LONGITUDE, longitude);
 			i.putExtra(RestaurantMap.EXTRA_NAME, name.getText().toString());
 			startActivity(i);
-			return(true);
+			return true;
 		}
-		return(super.onOptionsItemSelected(item));
+		return super.onOptionsItemSelected(item);
 	}
+	
 	private boolean isNetworkAvailable() {
-		ConnectivityManager cm=(ConnectivityManager)getActivity().
-				getSystemService(Context.CONNECTIVITY_SERVICE);
-		NetworkInfo info=cm.getActiveNetworkInfo();
-		return(info!=null);
+		ConnectivityManager cm = (ConnectivityManager)getActivity().getSystemService(Context.CONNECTIVITY_SERVICE);
+		NetworkInfo info = cm.getActiveNetworkInfo();
+		return info != null;
 	}
+	
 	private void load() {
-		Cursor c=helper.getById(restaurantId);
+		Cursor c = helper.getById(restaurantId);
 		c.moveToFirst();
 		name.setText(helper.getName(c));
 		address.setText(helper.getAddress(c));
 		notes.setText(helper.getNotes(c));
 		feed.setText(helper.getFeed(c));
+		
 		if (helper.getType(c).equals("sit_down")) {
 			types.check(R.id.sit_down);
 		}
@@ -131,52 +141,54 @@ public class DetailFragment extends Fragment {
 		else {
 			types.check(R.id.delivery);
 		}
-		latitude=helper.getLatitude(c);
-		longitude=helper.getLongitude(c);
-		location.setText(String.valueOf(latitude)
-				+", "
-				+String.valueOf(longitude));c.close();
+		latitude = helper.getLatitude(c);
+		longitude = helper.getLongitude(c);
+		location.setText(String.valueOf(latitude) + ", " + String.valueOf(longitude));
+		c.close();
 	}
+	
 	private void save() {
-		if (name.getText().toString().length()>0) {
-			String type=null;
+		if (name.getText().toString().length() > 0) {
+			String type = null;
+			
 			switch (types.getCheckedRadioButtonId()) {
 			case R.id.sit_down:
-				type="sit_down";
+				type = "sit_down";
 				break;
 			case R.id.take_out:
-				type="take_out";
+				type = "take_out";
 				break;
 			default:
-				type="delivery";
+				type = "delivery";
 				break;
 			}
-			if (restaurantId==null) {
+			
+			if (restaurantId == null) {
 				helper.insert(name.getText().toString(),
-						address.getText().toString(), type,
-						notes.getText().toString(),
-						feed.getText().toString());
+							  address.getText().toString(), 
+							  type,
+							  notes.getText().toString(),
+							  feed.getText().toString());
 			}
 			else {
-				helper.update(restaurantId, name.getText().toString(),
-						address.getText().toString(), type,
-						notes.getText().toString(),
-						feed.getText().toString());
+				helper.update(restaurantId, 
+							  name.getText().toString(),
+							  address.getText().toString(), 
+							  type,
+							  notes.getText().toString(),
+							  feed.getText().toString());
 			}
 		}
 	}
-	LocationListener onLocationChange=new LocationListener() {
+	
+	LocationListener onLocationChange = new LocationListener() {
 		public void onLocationChanged(Location fix) {
-			helper.updateLocation(restaurantId, fix.getLatitude(),
-					fix.getLongitude());
-			location.setText(String.valueOf(fix.getLatitude())
-					+", "
-					+String.valueOf(fix.getLongitude()));
+			helper.updateLocation(restaurantId, fix.getLatitude(), fix.getLongitude());
+			location.setText(String.valueOf(fix.getLatitude()) + ", " + String.valueOf(fix.getLongitude()));
 			locMgr.removeUpdates(onLocationChange);
 			Toast
-			.makeText(getActivity(), "Location saved",
-					Toast.LENGTH_LONG)
-					.show();
+				.makeText(getActivity(), "Location saved", Toast.LENGTH_LONG)
+				.show();
 		}
 		public void onProviderDisabled(String provider) {
 			// required for interface, not used
@@ -188,4 +200,5 @@ public class DetailFragment extends Fragment {
 			// required for interface, not used
 		}
 	};
+	
 }
